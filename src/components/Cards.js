7eaae28e5e901a2458cd57d73f95da3fe1dea8e8
GlazkovPavel <glazprom@mail.ru>
2021-08-04 15:10:49 +0300

    like

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,17 +8,34 @@ function Cards({ card, onCardClick }){
   function handleClick() {
     onCardClick(card)
   }
+
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = card.owner._id === currentUser._id;
+
+// Создаём переменную, которую после зададим в `className` для кнопки удаления
+  const cardDeleteButtonClassName = (
+      `element__trash ${isOwn ? '' : 'element__trash_hidden'}`
+  );
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+// Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = (
+      `element__description-like' ${isLiked ? 'element__description-like_active' : 'element__description-like'}`
+  );
+
   return(
 
         <li className="element__container">
           <div className="element__foto-container">
             <img className="element__foto" onClick={handleClick} src={card.link} alt={card.name} />
-            <button className="element__trash"></button>
+            <button className={cardDeleteButtonClassName}></button>
           </div>
           <div className="element__description">
             <h2 className="element__description-text">{card.name}</h2>
             <div className="element__container-like">
-              <button className="element__description-like" type="button"></button>
+              <button className={cardLikeButtonClassName} type="button"></button>
               <span className="element__likes">{card.likes.length}</span>
             </div>
           </div>
